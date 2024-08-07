0e9df0626e74cd92850eafbd1c6916f7864cd250
David Huang <djhuang636@gmail.com>
2024-08-07 13:23:26 -0400

    fix viewport sizing

---
+++ script.js
@@ -0,0 +1,4 @@
+let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+
+console.log(vh);
